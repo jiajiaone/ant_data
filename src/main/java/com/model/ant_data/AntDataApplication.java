d179578ee4a41ae39c20b670da3d34acba10ecbe
rujia.chen <986988596@qq>
2022-08-05 11:20:37 +0800

    init springboot model

---
+++ src/main/java/com/model/ant_data/AntDataApplication.java
@@ -0,0 +1,13 @@
+package com.model.ant_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/model/ant_data/AntDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.model.ant_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
