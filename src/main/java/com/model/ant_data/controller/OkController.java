3c821a33adc9a3384115089bf573082a165bb43c
chenrujia <986988596@qq.com>
2022-08-17 11:28:59 +0800

    1.test ok-08-17

--- src/main/java/com/model/ant_data/controller/OkController.java
+++ src/main/java/com/model/ant_data/controller/OkController.java
@@ -1,11 +1,16 @@
 package com.model.ant_data.controller;
 
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * 测试
  * @author chenrujia
  * @date 2022/07/26 16:07
  **/
+@RestController
+@RequestMapping("/okex")
 public class OkController {
 
 	/**
